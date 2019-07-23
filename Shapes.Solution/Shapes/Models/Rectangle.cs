dfd982dce3753a0aa40b2b3bf3369e57e761b73c
Student <example@example.com>
2019-07-23 16:37:59 -0700

    3 test are setup

---
+++ Shapes.Solution/Shapes/Models/Rectangle.cs
@@ -0,0 +1,33 @@
+namespace Shapes.Models
+{
+    //////inital class setup
+        public class Rectangle
+    {
+        public int Length {get; set;}
+        public int Width {get; set;}
+     ////////constructor setup
+        public Rectangle (int sideLength, int sideWidth)
+        {
+            Length = sideLength;
+            Width = sideWidth;
+        }
+        //////// calculates the area and determines if its a rectangle
+        public int GetArea()
+        {
+            return Length * Width;
+        }
+        ///// returns true/false if its a rectangle
+        public bool IsSquare()
+        {
+            if (Length == Width)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
