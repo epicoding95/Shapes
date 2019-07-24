d08bf09467fd9104d8c12f306129afa8917ff868
kelar crisp <kelcrisp007@icloud.com>
2019-07-24 00:22:08 -0400

    not finished

---
+++ Shapes.Solution/Shapes/Models/Cube.cs
@@ -0,0 +1,16 @@
+namespace Shapes.Models
+{
+  public class Cube
+  {
+    public Rectangle Face { get; set; }
+
+    public Cube (Rectangle side)
+    {
+      Face = side;
+    }
+    public int GetVolume()
+    {
+        return 0;
+    }
+  }
+}
